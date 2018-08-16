94550a50c8502dd8c763062e8a9686b460ed0f4d
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-16 22:01:02 +0700

    inisial comit

---
+++ src/jobsheet11/praktikum1.java
@@ -0,0 +1,8 @@
+package jobsheet11;
+import java.util.Locale;
+
+public class praktikum1 {
+    public static void main(String[] args)
+            {
+                String identitas = "MUHAMMAD FACHRI AFIF / X RPL 2 / 22";
+                System.out.println(" identitas : " + identitas);
