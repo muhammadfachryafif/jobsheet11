5827100b61b402de86089b0d88b5030d1c64e741
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-16 22:47:32 +0700

    Tambah Input.

--- src/jobsheet11/praktikum2.java
+++ src/jobsheet11/praktikum2.java
@@ -6,3 +6,10 @@
     public static void main(String[] args){    
         String identitas = " Muhammad Fachry Afif / X RPL 2 / 22 ";        
         tampilJudul(identitas); 
+ private static String tampilInput(){
+        Scanner scanner = new Scanner(System.in);        
+        System.out.println("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
