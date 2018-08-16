6413f193326b33ca56315e8a135c29721c820d7a
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-16 22:44:43 +0700

    Initial Commit.

---
+++ src/jobsheet11/praktikum2.java
@@ -0,0 +1,8 @@
+ private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    
+    public static void main(String[] args){    
+        String identitas = " Muhammad Fachry Afif / X RPL 2 / 22 ";        
+        tampilJudul(identitas); 
