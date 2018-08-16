605f2755cd9e70a1340cae4b030972b00d4cf64d
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-16 22:07:34 +0700

    coba method compareTo & compareTolgnoreCase

--- src/jobsheet11/praktikum1.java
+++ src/jobsheet11/praktikum1.java
@@ -13,3 +13,18 @@ public class praktikum1 {
                 String y = "";
                 System.out.println("isi variabel y = "+ y);
                 System.out.println("y adalah kosong = "+ y.isEmpty() );
+                 System.out.println("isi x sama dengan y = " + x.equals(y));
+        String z = "operasi";
+        System.out.println("isi variable z = " + z);
+        System.out.println("isi x sama dengan z (case sensitive) = " + x.equals(z));
+        String r = "operasi";
+        System.out.println("isi variable r = "+ r);
+        System.out.println("isi x sama dengan r (case sensitive = "+ x.equals(r));
+        System.out.println("isi x sama dengan r (Not case sensitive) = "+ x.equalsIgnoreCase(r));
+        System.out.println("perbandingan isi x dengan y = " + x.compareTo(y));
+        System.out.println("perbandingan isi x dengan z (case sensitive) + " + x.compareTo(z));
+        System.out.println("perbandingan isi x dengan r (case sensitive = " + x.compareTo(r));
+        System.out.println("perbandingan isi x dengan r (not case sensitive) = " + x.compareToIgnoreCase(r));
+        String s = "operasi";
+        System.out.println("isi variable s = "+ s);
+        System.out.println("perbandingan isi r dengan s (case sensitive) = "+ r.compareTo(s));
