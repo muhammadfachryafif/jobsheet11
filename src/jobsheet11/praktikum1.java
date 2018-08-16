2c6138e126e0c26f994e9ea1f50d0d5fe2b06353
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-16 22:03:05 +0700

    coba method length & isEmpty

--- src/jobsheet11/praktikum1.java
+++ src/jobsheet11/praktikum1.java
@@ -6,3 +6,10 @@ public class praktikum1 {
             {
                 String identitas = "MUHAMMAD FACHRI AFIF / X RPL 2 / 22";
                 System.out.println(" identitas : " + identitas);
+                 String x = "OPERASI";
+                System.out.println("isi variabel = " + x);
+                System.out.println("\""+ x +"\" panjang Character = "+ x.length () );
+                System.out.println("x adalah kosong = "+ x.isEmpty());
+                String y = "";
+                System.out.println("isi variabel y = "+ y);
+                System.out.println("y adalah kosong = "+ y.isEmpty() );
